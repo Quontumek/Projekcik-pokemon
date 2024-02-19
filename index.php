01884f16bea1557a9b9e56f075ba84925626047d
Phillip Christensen <144121553+Quontumek@users.noreply.github.com>
2024-02-19 18:37:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Le Pokémon pokédex</title>
+</head>
+<body>
+
+            
+
+<h1>Le Pokémon pokédex</h1>
+<ul>
+    <li>Login</li>
+    <li>Advance Search</li>
+</ul>    
+
+<nav>
+    <div>       
+
+    </div>
+</nav>
+
+</body>
+</html>
