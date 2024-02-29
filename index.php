5a41ae6d32131e244a16e61b8f92a25da567f72f
Phillip Christensen <144121553+Quontumek@users.noreply.github.com>
2024-02-29 15:35:12 +0100

    Add files via upload
    o jezu ratuj mnie

--- index.php
+++ index.php
@@ -3,22 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Le Pokémon pokédex</title>
+    <link rel="stylesheet" href="style.css?v=1">
+    <title>Pokémon creator/dex</title>
 </head>
 <body>
 
-            
 
-<h1>Le Pokémon pokédex</h1>
+<h1>Pokémon creator/dex </h1>
 <ul>
-    <li>Login</li>
-    <li>Advance Search</li>
+    <li><a target="_blank" href="https://monolit.onrender.com/auth?destination=https://localhost/Pokémon/loginreceiver.php&clientid=018df488-c521-7477-9a00-970921f3fd91">Login</a></li> 
+
+    <li><a href="AdvSearch.php">Advanced Search</li>
+    
+    <li><a href="Creator.php">Creator<li>
+
 </ul>    
 
 <nav>
     <div>       
-
+    
     </div>
 </nav>
 
