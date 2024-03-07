a94bebe9369c7198147efef322cac39d14ef3831
Phillip Christensen <144121553+Quontumek@users.noreply.github.com>
2024-03-07 14:54:12 +0100

    ughh nowy css

--- index.php
+++ index.php
@@ -4,26 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css?v=1">
-    <title>Pokémon creator/dex</title>
+    <title>Pokémon projekt</title>
 </head>
 <body>
 
+<div id="pasekL">
 
-<h1>Pokémon creator/dex </h1>
-<ul>
-    <li><a target="_blank" href="https://monolit.onrender.com/auth?destination=https://localhost/Pokémon/loginreceiver.php&clientid=018df488-c521-7477-9a00-970921f3fd91">Login</a></li> 
+</div>
+<div id="pasekR">
 
-    <li><a href="AdvSearch.php">Advanced Search</li>
-    
-    <li><a href="Creator.php">Creator<li>
+</div>
 
-</ul>    
+<h1>Pokémon dex-pass</h1>
+
+<div id="menu">
+        <ul>
+            <li><a href="https://monolit.onrender.com/auth?destination=https://localhost/Pokémon/loginreceiver.php&clientid=018df488-c521-7477-9a00-970921f3fd91">Login</a></li> 
+|
+            <li>Advanced Search</li>
+|   
+            <li>Would, wouldn't<li>
+        </ul>  
+</div>
 
 <nav>
-    <div>       
-    
+    <div> 
     </div>
 </nav>
+
+
+
 
 </body>
 </html>
